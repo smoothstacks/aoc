cde97cd67a2762caa43cd081bb79fb1b868b4b7c
smoothstacks <smoothstacks@proton.me>
2023-12-09 21:21:00 +0000

    days 7/8

---
+++ days/day7/src/lib.rs
@@ -0,0 +1,168 @@
+use std::collections::HashMap;
+
+mod parse {
+    use nom::{
+        character::complete::{anychar, digit1, newline, space1},
+        combinator::{map_opt, map_res},
+        multi::{count, separated_list1},
+        sequence::separated_pair,
+        IResult,
+    };
+
+    use super::*;
+
+    impl Card {
+        pub fn parse(input: &str) -> IResult<&str, Card> {
+            map_opt(anychar, |label| {
+                Card::ALL
+                    .iter()
+                    .position(|other| *other == label)
+                    .map(|rank| Card { label, rank })
+            })(input)
+        }
+    }
+
+    impl Hand {
+        pub fn parse(input: &str) -> IResult<&str, Hand> {
+            let (input, cards_vec) = count(Card::parse, 5)(input)?;
+            let mut cards = [Card::default(); 5];
+            cards.copy_from_slice(&cards_vec);
+            Ok((input, Hand::new(cards)))
+        }
+    }
+
+    pub fn parse(input: &str) -> IResult<&str, Vec<(Hand, u32)>> {
+        separated_list1(
+            newline,
+            separated_pair(Hand::parse, space1, map_res(digit1, str::parse::<u32>)),
+        )(input)
+    }
+}
+
+#[derive(Copy, Clone, Default, PartialEq, Eq)]
+struct Card {
+    label: char,
+    rank: usize,
+}
+
+impl std::fmt::Debug for Card {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "{}", self.label)
+    }
+}
+
+impl PartialOrd for Card {
+    fn partial_cmp(&self, other: &Self) -> Option<std::cmp::Ordering> {
+        Some(self.cmp(other))
+    }
+}
+
+impl Ord for Card {
+    fn cmp(&self, other: &Self) -> std::cmp::Ordering {
+        self.rank.cmp(&other.rank)
+    }
+}
+
+impl Card {
+    const ALL: [char; 13] = [
+        '2', '3', '4', '5', '6', '7', '8', '9', 'T', 'J', 'Q', 'K', 'A',
+    ];
+}
+
+#[derive(PartialEq, Eq)]
+struct Hand {
+    cards: [Card; 5],
+
+    distinct_labels: usize,
+    max_label_count: usize,
+    label_counts: HashMap<char, usize>,
+}
+
+impl Hand {
+    pub fn new(cards: [Card; 5]) -> Self {
+        let mut label_counts = HashMap::new();
+        for card in &cards {
+            label_counts
+                .entry(card.label)
+                .and_modify(|v| *v += 1)
+                .or_insert(1);
+        }
+
+        let distinct_labels = label_counts.len();
+        let max_label_count = *label_counts.values().max().unwrap();
+
+        Self {
+            cards,
+            label_counts,
+            distinct_labels,
+            max_label_count,
+        }
+    }
+}
+
+impl PartialOrd for Hand {
+    fn partial_cmp(&self, other: &Self) -> Option<std::cmp::Ordering> {
+        Some(self.cmp(other))
+    }
+}
+
+impl Ord for Hand {
+    fn cmp(&self, other: &Self) -> std::cmp::Ordering {
+        other
+            .distinct_labels
+            .cmp(&self.distinct_labels)
+            .then(self.max_label_count.cmp(&other.max_label_count))
+            .then(self.cards.cmp(&other.cards))
+    }
+}
+
+impl std::fmt::Debug for Hand {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        for card in &self.cards {
+            write!(f, "{:?}", card)?;
+        }
+        Ok(())
+    }
+}
+
+pub fn part1(input: &str) -> u32 {
+    let (_, mut hands) = parse::parse(input).expect("parse works");
+    hands.sort_by(|a, b| a.0.cmp(&b.0));
+    hands
+        .iter()
+        .enumerate()
+        .map(|(i, (_, bid))| bid * (i + 1) as u32)
+        .sum()
+}
+
+pub fn part2(input: &str) -> u32 {
+    0
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::Hand;
+
+    const INPUT: &str = "32T3K 765
+T55J5 684
+KK677 28
+KTJJT 220
+QQQJA 483";
+
+    #[test]
+    fn part1_works() {
+        assert_eq!(super::part1(INPUT), 6440);
+    }
+    #[test]
+    fn part2_works() {
+        assert_eq!(super::part2(INPUT), 0);
+    }
+
+    #[test]
+    fn hand_tests() {
+        let first = Hand::parse("33332").unwrap().1;
+        let second = Hand::parse("2AAAA").unwrap().1;
+
+        assert!(first > second);
+    }
+}
---
+++ days/day8/src/lib.rs
@@ -0,0 +1,186 @@
+use std::collections::HashMap;
+
+mod parse {
+
+    use nom::{
+        bytes::complete::{tag, take},
+        character::complete::{anychar, newline},
+        combinator::map_opt,
+        multi::{many1, separated_list1},
+        sequence::{delimited, separated_pair},
+        IResult,
+    };
+
+    use super::*;
+
+    fn parse_direction(input: &str) -> IResult<&str, Direction> {
+        map_opt(anychar, |c| match c {
+            'L' => Some(Direction::Left),
+            'R' => Some(Direction::Right),
+            _ => None,
+        })(input)
+    }
+
+    fn parse_node(input: &str) -> IResult<&str, &str> {
+        take(3usize)(input)
+    }
+
+    fn parse_node_pair(input: &str) -> IResult<&str, (&str, &str)> {
+        delimited(
+            tag("("),
+            separated_pair(parse_node, tag(", "), parse_node),
+            tag(")"),
+        )(input)
+    }
+
+    fn parse_map_entry(input: &str) -> IResult<&str, (&str, (&str, &str))> {
+        separated_pair(parse_node, tag(" = "), parse_node_pair)(input)
+    }
+
+    pub fn parse(input: &str) -> IResult<&str, (Map<'_>, Vec<Direction>)> {
+        let (input, (directions, entries)) = separated_pair(
+            many1(parse_direction),
+            tag("\n\n"),
+            separated_list1(newline, parse_map_entry),
+        )(input)?;
+
+        let map: HashMap<&str, [&str; 2]> = entries
+            .into_iter()
+            .map(|(k, (v1, v2))| (k, [v1, v2]))
+            .collect();
+        Ok((input, (Map(map), directions)))
+    }
+}
+
+const START: &str = "AAA";
+const END: &str = "ZZZ";
+
+#[repr(u8)]
+#[derive(Debug, Clone, Copy)]
+enum Direction {
+    Left,
+    Right = 1,
+}
+
+struct Map<'a>(HashMap<&'a str, [&'a str; 2]>);
+impl Map<'_> {
+    fn path_length(
+        &self,
+        start: &str,
+        directions: &[Direction],
+        end: impl Fn(&str) -> bool,
+    ) -> usize {
+        let mut direction = 0;
+        let mut len = 0;
+        let mut current = start;
+        while !end(current) {
+            current = self
+                .0
+                .get(current)
+                .map(|options| options[directions[direction] as usize])
+                .unwrap();
+
+            assert!(current != "XXX");
+
+            direction = (direction + 1) % directions.len();
+            len += 1;
+        }
+
+        len
+    }
+
+    fn follow_paths_2(&self, directions: &[Direction]) -> usize {
+        self.0
+            .keys()
+            .copied()
+            .filter_map(|s| {
+                if s.ends_with("A") {
+                    Some(self.path_length(s, &directions, |o| o.ends_with("Z")))
+                } else {
+                    None
+                }
+            })
+            .inspect(|p| println!("{p}"))
+            .product()
+    }
+
+    fn follow_paths(&self, directions: &[Direction]) -> usize {
+        let mut start_nodes: Vec<&str> = self
+            .0
+            .keys()
+            .copied()
+            .filter(|s| s.ends_with("A"))
+            .collect();
+
+        let mut len = 0;
+        let mut direction = 0;
+        loop {
+            if start_nodes[2..3].iter().all(|s| s.ends_with("Z")) {
+                break;
+            }
+
+            for node in &mut start_nodes[2..3] {
+                *node = self
+                    .0
+                    .get(*node)
+                    .map(|options| options[directions[direction] as usize])
+                    .unwrap();
+            }
+
+            direction = (direction + 1) % directions.len();
+            len += 1
+        }
+
+        len
+    }
+}
+
+pub fn part1(input: &str) -> u32 {
+    let (_, (map, directions)) = parse::parse(input).expect("parse works");
+    map.path_length(START, &directions, |s| s == END) as u32
+}
+pub fn part2(input: &str) -> u32 {
+    let (_, (map, directions)) = parse::parse(input).expect("parse works");
+    map.follow_paths_2(&directions) as u32
+}
+
+#[cfg(test)]
+mod tests {
+
+    #[test]
+    fn part1_works() {
+        const INPUT_1: &str = "RL
+
+AAA = (BBB, CCC)
+BBB = (DDD, EEE)
+CCC = (ZZZ, GGG)
+DDD = (DDD, DDD)
+EEE = (EEE, EEE)
+GGG = (GGG, GGG)
+ZZZ = (ZZZ, ZZZ)";
+
+        const INPUT_2: &str = "LLR
+
+AAA = (BBB, BBB)
+BBB = (AAA, ZZZ)
+ZZZ = (ZZZ, ZZZ)";
+
+        assert_eq!(super::part1(INPUT_1), 2);
+        assert_eq!(super::part1(INPUT_2), 6);
+    }
+    #[test]
+    fn part2_works() {
+        const INPUT: &str = "LR
+
+11A = (11B, XXX)
+11B = (XXX, 11Z)
+11Z = (11B, XXX)
+22A = (22B, XXX)
+22B = (22C, 22C)
+22C = (22Z, 22Z)
+22Z = (22B, 22B)
+XXX = (XXX, XXX)";
+
+        assert_eq!(super::part2(INPUT), 6);
+    }
+}
